5a01e6e3ad6906895e5e4226573e2e59b108850c
mistychris6 <31109593+mistychris6@users.noreply.github.com>
2020-06-22 11:07:42 -0600

    Create index.js

---
+++ WhosBuyingBrunch/index.js
@@ -0,0 +1,9 @@
+let coolCuzClub = ["Misty", "Margarita", "Derrick", "Kelly"]
+
+function whosPaying(coolCuzClub) {
+    let randoNum = Math.floor((Math.random() * 4))
+    return coolCuzClub[randoNum] + " is going to buy brunch today!"
+   
+}
+ 
+ whosPaying(coolCuzClub)
